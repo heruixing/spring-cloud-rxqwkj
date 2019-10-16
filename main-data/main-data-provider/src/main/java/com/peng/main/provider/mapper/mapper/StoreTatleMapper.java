74eae94d8d0270416aeed8e8e00744e304cc1c1a
heruixing <admin@heruixing.cn>
2019-10-17 01:44:10 +0800

    Test店铺信息

---
+++ main-data/main-data-provider/src/main/java/com/peng/main/provider/mapper/mapper/StoreTatleMapper.java
@@ -0,0 +1,11 @@
+package com.peng.main.provider.mapper.mapper;
+
+import com.peng.main.api.mapper.model.StoreTatle;
+import tk.mybatis.mapper.common.Mapper;
+
+/**
+ * Created by Administrator on 2019/10/16.
+ */
+@org.apache.ibatis.annotations.Mapper
+public interface StoreTatleMapper extends Mapper<StoreTatle> {
+}
